bd0ca0f3ac48a93c185cb6a34bf9588117b013d8
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2023-09-16 23:55:30 +0600

    com

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Calculator from "./Components/Calculator";
-import Text from "./Components/Inheritance/Text";
+import Bracket from "./Components/Inheritance/Composition/Bracket";
+import Emoji from "./Components/Inheritance/Composition/Emoji";
+import Text from "./Components/Inheritance/Composition/Text";
 
 function App() {
   return (
     <div className="App">
       <Calculator />
-      <Text></Text>
+      <Emoji>
+        {({ addEmoji }) => (
+          <Bracket>
+            {({ addBracket }) => (
+              <Text addEmoji={addEmoji} addBracket={addBracket} />
+            )}
+          </Bracket>
+        )}
+      </Emoji>
     </div>
   );
 }
